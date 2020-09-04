53db7fbecfe727e4cfd1a42492724c967b855dac
Sindre Wilting <sindrewilting@gmail.com>
2020-09-04 12:40:32 +0200

    Start on implementing functions

--- Source/events/EventStore.ts
+++ Source/events/EventStore.ts
@@ -4,16 +4,17 @@
 import { Logger } from 'winston';
 import { map } from 'rxjs/operators';
 
-import { callContexts, failures } from '@dolittle/sdk.protobuf';
+import { callContexts, failures, guids } from '@dolittle/sdk.protobuf';
 import { ArtifactId, Artifact, IArtifacts } from '@dolittle/sdk.artifacts';
 import { IExecutionContextManager } from '@dolittle/sdk.execution';
 import { Cancellation } from '@dolittle/sdk.resilience';
 import { reactiveUnary } from '@dolittle/sdk.services';
 
 import { EventStoreClient } from '@dolittle/runtime.contracts/Runtime/Events/EventStore_grpc_pb';
-import { CommitEventsRequest, CommitEventsResponse as PbCommitEventsResponse } from '@dolittle/runtime.contracts/Runtime/Events/EventStore_pb';
+import { CommitEventsRequest, CommitEventsResponse as PbCommitEventsResponse, CommitAggregateEventsRequest } from '@dolittle/runtime.contracts/Runtime/Events/EventStore_pb';
+import { UncommittedAggregateEvents as PbUncommittedAggregateEvents } from '@dolittle/runtime.contracts/Runtime/Events/Uncommitted_pb';
 
-import { CommittedEvents, IEventStore, EventSourceId, UncommittedEvent, EventConverters, CommitEventsResponse } from './index';
+import { CommittedEvents, IEventStore, EventSourceId, UncommittedEvent, EventConverters, CommitEventsResponse, UncommittedAggregateEvents, UncommittedAggregateEvent } from './index';
 import { Guid } from '@dolittle/rudiments';
 
 /**
@@ -47,6 +48,27 @@ export class EventStore implements IEventStore {
     }
 
     /** @inheritdoc */
+    commitForAggregate(event: any, eventSourceId: Guid | string, aggregateRoot: Function, expectedAggregateRootVersion: number, artifact?: Artifact | Guid | string, cancellation?: Cancellation): Promise<CommitEventsResponse>;
+    commitForAggregate(events: UncommittedAggregateEvents, cancellation?: Cancellation): Promise<CommitEventsResponse>;
+    commitForAggregate(eventOrEvents: any, eventSourceIdOrCancellation?: Guid | string | Cancellation, aggregateRoot?: Function, expectedAggregateRootVersion?: number, artifact?: Artifact | Guid | string, cancellation?: Cancellation): Promise<CommitEventsResponse> {
+        if (this.isUncommittedAggregateEvents(eventOrEvents)) {
+            return this.commitForAggregateInternal(eventOrEvents, eventSourceIdOrCancellation as Cancellation);
+        }
+        const eventSourceId = eventSourceIdOrCancellation as Guid | string;
+        return this.commitForAggregateInternal(
+            UncommittedAggregateEvents.from(
+                eventSourceId,
+                aggregateRoot!,
+                expectedAggregateRootVersion!,
+                {
+                    content: eventOrEvents,
+                    artifact: artifact instanceof Artifact ? artifact : ArtifactId.from(artifact!),
+                    public: false
+                }),
+            cancellation);
+    }
+
+    /** @inheritdoc */
     commitPublic(event: any, eventSourceId: Guid | string, artifact?: Artifact | Guid | string, cancellation?: Cancellation): Promise<CommitEventsResponse> {
         const events: UncommittedEvent[] = [this.toUncommittedEvent(event, eventSourceId, artifact, true)];
         return this.commitInternal(events, cancellation);
@@ -54,6 +76,10 @@ export class EventStore implements IEventStore {
 
     private isArrayOfUncommittedEvents(eventOrEvents: any): eventOrEvents is UncommittedEvent[] {
         return Array.isArray(eventOrEvents) && eventOrEvents.length > 0 && eventOrEvents[0].eventSourceId && eventOrEvents[0].content;
+    }
+
+    private isUncommittedAggregateEvents(eventOrEvents: any): eventOrEvents is UncommittedAggregateEvents {
+        return eventOrEvents instanceof UncommittedAggregateEvents && eventOrEvents.toArray().length > 0;
     }
 
     private async commitInternal(events: UncommittedEvent[], cancellation = Cancellation.default): Promise<CommitEventsResponse> {
@@ -70,6 +96,31 @@ export class EventStore implements IEventStore {
 
         return reactiveUnary(this._eventStoreClient, this._eventStoreClient.commit, request, cancellation)
             .pipe(map(response => {
+                const committedEvents = new CommittedEvents(...response.getEventsList().map(event => EventConverters.toSDK(event)));
+                return new CommitEventsResponse(committedEvents, failures.toSDK(response.getFailure()));
+            })).toPromise();
+    }
+    private async commitAggregateInternal(events: UncommittedAggregateEvents, cancellation = Cancellation.default): Promise<CommitEventsResponse> {
+        const uncommittedAggregateEvents: PbUncommittedAggregateEvents.UncommittedAggregateEvent[] = events.toArray().map(event =>
+            EventConverters.getUncommittedAggregateEventFrom(
+                event.content,
+                this._artifacts.resolveFrom(event.content, event.artifact),
+                !!event.public));
+
+        const aggregateRoot = this._artifacts.getFor(events.aggregateRoot as any);
+        const request = new CommitAggregateEventsRequest();
+        const pbEvents = new PbUncommittedAggregateEvents();
+        pbEvents.setEventsList(uncommittedAggregateEvents);
+        pbEvents.setAggregaterootid(guids.toProtobuf(aggregateRoot.id.value));
+        pbEvents.setEventsourceid(guids.toProtobuf(events.eventSourceId.value));
+        pbEvents.setExpectedaggregaterootversion(events.expectedAggregateRootVersion);
+        request.setCallcontext(callContexts.toProtobuf(this._executionContextManager.current));
+        request.setEvents(pbEvents);
+
+        return reactiveUnary(this._eventStoreClient, this._eventStoreClient.commitForAggregate, request, cancellation)
+            .pipe(map(response => {
+                const events = response.getEvents();
+                const failure = response.getFailure();
                 const committedEvents = new CommittedEvents(...response.getEventsList().map(event => EventConverters.toSDK(event)));
                 return new CommitEventsResponse(committedEvents, failures.toSDK(response.getFailure()));
             })).toPromise();
